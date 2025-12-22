224002d473703f5bc9068108cec0256c38f7faea
ryu-hyesu <aimee3994@naver.com>
2025-12-22 22:10:29 +0900

    feat: 나의 고명 Entitiy 설계 (#5)

--- src/main/java/com/advent/backend/entity/MyTteok.java
+++ src/main/java/com/advent/backend/entity/MyTteok.java
@@ -1,5 +1,25 @@
 package com.advent.backend.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
 // 나의 떡국
+@Entity
+@Table(name = "my_tteoks")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MyTteok {
+    // 나의 떡국 ID
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    // 유저 ID
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
