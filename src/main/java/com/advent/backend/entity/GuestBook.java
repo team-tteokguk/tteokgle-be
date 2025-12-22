3d9a8a7c91e127e2c19292d5a154663abf8e7bca
ryu-hyesu <aimee3994@naver.com>
2025-12-22 17:39:10 +0900

    feat: guest entity 중간 설계 (#5)

--- src/main/java/com/advent/backend/entity/GuestBook.java
+++ src/main/java/com/advent/backend/entity/GuestBook.java
@@ -1,5 +1,19 @@
 package com.advent.backend.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
 // 방명록
-public class GuestBook {
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GuestBook extends BaseTimeEntity {
+    // 방명록 ID
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 }
