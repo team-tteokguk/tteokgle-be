89f9ce81e797ed20b6a9de9ddaa190861361c108
ryu-hyesu <aimee3994@naver.com>
2025-12-22 22:08:19 +0900

    feat: 공유 링크 Entitiy 설계 (#5)

--- src/main/java/com/advent/backend/entity/ShareLink.java
+++ src/main/java/com/advent/backend/entity/ShareLink.java
@@ -1,5 +1,30 @@
 package com.advent.backend.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
 // 공유 링크
-public class ShareLink {
+@Entity
+@Table(name = "share_links")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ShareLink extends BaseTimeEntity {
+    // 공유 링크 ID
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    // 상점 ID
+    @ManyToOne
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    // URL에 붙일 토큰
+    @Column(nullable = false)
+    private String token;
 }
