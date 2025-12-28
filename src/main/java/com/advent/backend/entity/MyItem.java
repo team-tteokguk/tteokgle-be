cede66b05178736eced68dd68afdcf7051fe1b26
ryu-hyesu <aimee3994@naver.com>
2025-12-28 15:04:50 +0900

    feat: 나의 고명 Entitiy 설계 (#5)

--- src/main/java/com/advent/backend/entity/MyItem.java
+++ src/main/java/com/advent/backend/entity/MyItem.java
@@ -1,9 +1,46 @@
 package com.advent.backend.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
 
 // 나의 고명
 @Entity
+@Table(name = "my_items")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MyItem {
+    // 내 고명 ID
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    // 떡국 ID
+    @ManyToOne
+    @JoinColumn(name = "tteok_id")
+    private MyTteok tteok;
+
+    // 고명 아이템 ID
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    // 컨텐츠 확인 여부
+    @Column(columnDefinition = "integer default false", nullable = false)
+    private boolean isRead = false;
+
+    // 고명 올림 여부
+    @Column(columnDefinition = "integer default false", nullable = false)
+    private boolean isUsed = false;
+
+    // 나의 고명 x, y, z 위치
+    @Column
+    private Float pos_x;
+    @Column
+    private Float pos_y;
+    @Column
+    private Float pos_z;
 }
