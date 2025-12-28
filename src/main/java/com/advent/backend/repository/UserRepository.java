03afad9fc285426c2267cc2289a28e367498e0dc
ryu-hyesu <aimee3994@naver.com>
2025-12-28 20:42:01 +0900

    feat: User Repository 인터페이스 생성 (#7)

---
+++ src/main/java/com/advent/backend/repository/UserRepository.java
@@ -0,0 +1,28 @@
+package com.advent.backend.repository;
+
+import com.advent.backend.entity.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public class UserRepository {
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(User user) {
+        em.persist(user);
+    }
+
+    public User findOne(UUID id) {
+        return em.find(User.class, id);
+    }
+
+    public List<User> findAll() {
+        return em.createQuery("select u from User u", User.class)
+                .getResultList();
+    }
+}
