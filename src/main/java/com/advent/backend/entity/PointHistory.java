99aee4896633b7c1a00baa47e5efc4044517d793
YEJI JUNG <yejiii1997@gmail.com>
2025-12-28 20:29:18 +0900

    feat: 금전 기록 엔티티 추가 (#5)

--- src/main/java/com/advent/backend/entity/PointHistory.java
+++ src/main/java/com/advent/backend/entity/PointHistory.java
@@ -1,5 +1,48 @@
 package com.advent.backend.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 // 금전 기록
-public class PointHistory {
+@Entity
+@Table(name = "point-history-tbl")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PointHistory extends BaseTimeEntity {
+
+    public enum TradeType {
+        CHARGE,
+        USE,
+        REFUND
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "history_id")
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id", nullable = false)
+    private User receiver;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Store item;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "trade_type", nullable = false)
+    private TradeType tradeType;
+
+    @Column(name = "amount", nullable = false)
+    private Integer amount;
+
 }
