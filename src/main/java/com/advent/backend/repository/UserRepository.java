1242e39e226efbd204891e8bbc9aaeed7dd16ab6
ryu-hyesu <aimee3994@naver.com>
2025-12-29 15:21:11 +0900

    _

--- src/main/java/com/advent/backend/repository/UserRepository.java
+++ src/main/java/com/advent/backend/repository/UserRepository.java
@@ -3,26 +3,16 @@ package com.advent.backend.repository;
 import com.advent.backend.entity.Member;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.apache.catalina.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public class UserRepository {
-    @PersistenceContext
-    private EntityManager em;
-
-    public void save(Member member) {
-        em.persist(member);
-    }
-
-    public Member findOne(UUID id) {
-        return em.find(Member.class, id);
-    }
-
-    public List<Member> findAll() {
-        return em.createQuery("select u from Member u", Member.class)
-                .getResultList();
-    }
+public interface UserRepository extends JpaRepository<User, UUID> {
+    // 소셜 타입과 소셜 아이디 조합으로 유저 검색
+    Optional<User> findBySocialIdAndSocialType(String socialId, Member.SocialType socialType);
 }
