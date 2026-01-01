14a94056014cceb3b619da6ac71a497fe6918c4b
ryu-hyesu <aimee3994@naver.com>
2026-01-01 20:30:51 +0900

    feat: NotificationRepository에서 안 쓰는 코드 정리 (#11)

--- src/main/java/com/advent/backend/repository/NotificationRepository.java
+++ src/main/java/com/advent/backend/repository/NotificationRepository.java
@@ -2,12 +2,11 @@ package com.advent.backend.repository;
 
 import com.advent.backend.entity.Notification;
 import jakarta.transaction.Transactional;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-import java.util.UUID;
 
 public interface NotificationRepository extends JpaRepository<Notification, UUID> {
     // 한번에 알림 읽음 메소드
--- src/test/java/com/advent/backend/repository/NotificationRepositoryTest.java
+++ src/test/java/com/advent/backend/repository/NotificationRepositoryTest.java
@@ -1,36 +1,27 @@
 package com.advent.backend.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.advent.backend.entity.Member;
 import com.advent.backend.entity.Notification;
-import org.aspectj.weaver.ast.Not;
+import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-
 import org.springframework.data.domain.Pageable;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.data.domain.Sort;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class NotificationRepositoryTest {
-    @Autowired
-    private NotificationRepository notificationRepository;
+    @Autowired private NotificationRepository notificationRepository;
 
-    @Autowired
-    private MemberRepository memberRepository;
-
-    @Autowired
-    private TestEntityManager entityManager; // 혹은 EntityManager
+    @Autowired private MemberRepository memberRepository;
 
     @Test
     @DisplayName("알림 생성")
@@ -38,19 +29,17 @@ public class NotificationRepositoryTest {
         String socialId = "unique_social_id_123";
         Member.SocialType socialType = Member.SocialType.KAKAO;
 
-        Member member = Member.builder()
-                .socialId(socialId)
-                .socialType(socialType)
-                .nickname("외요3")
-                .build();
+        Member member =
+                Member.builder().socialId(socialId).socialType(socialType).nickname("외요3").build();
 
         memberRepository.save(member);
 
-        Notification notification = Notification.builder()
-                .member(member)
-                .NotificationType(Notification.NotificationType.COMMENT)
-                .message("네 고명에 큰 인상을 받았어")
-                .build();
+        Notification notification =
+                Notification.builder()
+                        .member(member)
+                        .NotificationType(Notification.NotificationType.COMMENT)
+                        .message("네 고명에 큰 인상을 받았어")
+                        .build();
 
         notificationRepository.save(notification);
 
@@ -58,7 +47,8 @@ public class NotificationRepositoryTest {
 
         assertThat(result.isPresent()).isTrue();
         assertThat(result.get()).isEqualTo(notification);
-        assertThat(result.get().getNotificationType()).isEqualTo(Notification.NotificationType.COMMENT);
+        assertThat(result.get().getNotificationType())
+                .isEqualTo(Notification.NotificationType.COMMENT);
         assertThat(result.get().getMessage()).isEqualTo(notification.getMessage());
         assertThat(result.get().getId()).isEqualTo(notification.getId());
     }
@@ -69,42 +59,41 @@ public class NotificationRepositoryTest {
         String socialId = "unique_social_id_123";
         Member.SocialType socialType = Member.SocialType.KAKAO;
 
-        Member member = Member.builder()
-                .socialId(socialId)
-                .socialType(socialType)
-                .nickname("외요3")
-                .build();
+        Member member =
+                Member.builder().socialId(socialId).socialType(socialType).nickname("외요3").build();
 
         memberRepository.save(member);
 
-        Notification notification = Notification.builder()
-                .member(member)
-                .NotificationType(Notification.NotificationType.COMMENT)
-                .message("네 고명에 큰 인상을 받았어")
-                .build();
+        Notification notification =
+                Notification.builder()
+                        .member(member)
+                        .NotificationType(Notification.NotificationType.COMMENT)
+                        .message("네 고명에 큰 인상을 받았어")
+                        .build();
 
-        Notification notification2 = Notification.builder()
-                .member(member)
-                .NotificationType(Notification.NotificationType.SALE)
-                .message("a회원이 님 고명을 구매했어요")
-                .build();
+        Notification notification2 =
+                Notification.builder()
+                        .member(member)
+                        .NotificationType(Notification.NotificationType.SALE)
+                        .message("a회원이 님 고명을 구매했어요")
+                        .build();
 
-        Notification notification3 = Notification.builder()
-                .member(member)
-                .NotificationType(Notification.NotificationType.SUBSCRIBE)
-                .message("a회원이 님 상점을 구독했어요")
-                .build();
+        Notification notification3 =
+                Notification.builder()
+                        .member(member)
+                        .NotificationType(Notification.NotificationType.SUBSCRIBE)
+                        .message("a회원이 님 상점을 구독했어요")
+                        .build();
 
-        Notification notification4 = Notification.builder()
-                .member(member)
-                .NotificationType(Notification.NotificationType.SUBSCRIBE)
-                .message("전체 회원들한테 공지 드림")
-                .build();
+        Notification notification4 =
+                Notification.builder()
+                        .member(member)
+                        .NotificationType(Notification.NotificationType.SUBSCRIBE)
+                        .message("전체 회원들한테 공지 드림")
+                        .build();
 
-        notificationRepository.saveAllAndFlush(List.of(notification, notification2, notification3, notification4));
-
-//        entityManager.flush();
-
+        notificationRepository.saveAllAndFlush(
+                List.of(notification, notification2, notification3, notification4));
         notificationRepository.updateNotification(member.getId());
         List<Notification> allNotifications = notificationRepository.findAll();
         assertThat(allNotifications).allMatch(Notification::isRead);
@@ -116,39 +105,41 @@ public class NotificationRepositoryTest {
         String socialId = "unique_social_id_123";
         Member.SocialType socialType = Member.SocialType.KAKAO;
 
-        Member member = Member.builder()
-                .socialId(socialId)
-                .socialType(socialType)
-                .nickname("외요3")
-                .build();
+        Member member =
+                Member.builder().socialId(socialId).socialType(socialType).nickname("외요3").build();
 
         memberRepository.save(member);
 
-        Notification notification = Notification.builder()
-                .member(member)
-                .NotificationType(Notification.NotificationType.COMMENT)
-                .message("네 고명에 큰 인상을 받았어")
-                .build();
+        Notification notification =
+                Notification.builder()
+                        .member(member)
+                        .NotificationType(Notification.NotificationType.COMMENT)
+                        .message("네 고명에 큰 인상을 받았어")
+                        .build();
 
-        Notification notification2 = Notification.builder()
-                .member(member)
-                .NotificationType(Notification.NotificationType.SALE)
-                .message("a회원이 님 고명을 구매했어요")
-                .build();
+        Notification notification2 =
+                Notification.builder()
+                        .member(member)
+                        .NotificationType(Notification.NotificationType.SALE)
+                        .message("a회원이 님 고명을 구매했어요")
+                        .build();
 
-        Notification notification3 = Notification.builder()
-                .member(member)
-                .NotificationType(Notification.NotificationType.SUBSCRIBE)
-                .message("a회원이 님 상점을 구독했어요")
-                .build();
+        Notification notification3 =
+                Notification.builder()
+                        .member(member)
+                        .NotificationType(Notification.NotificationType.SUBSCRIBE)
+                        .message("a회원이 님 상점을 구독했어요")
+                        .build();
 
-        Notification notification4 = Notification.builder()
-                .member(member)
-                .NotificationType(Notification.NotificationType.ALARM)
-                .message("전체 회원들한테 공지 드림")
-                .build();
+        Notification notification4 =
+                Notification.builder()
+                        .member(member)
+                        .NotificationType(Notification.NotificationType.ALARM)
+                        .message("전체 회원들한테 공지 드림")
+                        .build();
 
-        notificationRepository.saveAll(List.of(notification, notification2, notification3, notification4));
+        notificationRepository.saveAll(
+                List.of(notification, notification2, notification3, notification4));
 
         Pageable pageable = PageRequest.of(0, 2, Sort.by("createdAt").descending());
 
@@ -156,6 +147,5 @@ public class NotificationRepositoryTest {
 
         assertThat(page.getContent().size()).isEqualTo(2);
         assertThat(page.getTotalElements()).isEqualTo(4);
-
     }
 }
