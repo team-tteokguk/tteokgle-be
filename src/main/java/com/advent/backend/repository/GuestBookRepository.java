22f00d28822f55aab410f91437c1ee95cdfe0b3d
YEJI JUNG <yejiii1997@gmail.com>
2026-01-02 01:14:52 +0900

    feat: Guestbook 레포지토리 구현 (#17)

---
+++ src/main/java/com/advent/backend/repository/GuestBookRepository.java
@@ -0,0 +1,24 @@
+package com.advent.backend.repository;
+
+import com.advent.backend.entity.GuestBook;
+import com.advent.backend.entity.Store;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface GuestBookRepository extends JpaRepository<GuestBook, UUID> {
+
+    Page<GuestBook> findAllByStore(Store store, Pageable pageable);
+
+    @Query("SELECT g FROM GuestBook g JOIN FETCH g.member WHERE g.store.id = :storeId")
+    Page<GuestBook> findAllByStoreIdWithMember(@Param("storeId") UUID storeId, Pageable pageable);
+
+    @Query("SELECT g FROM GuestBook g JOIN FETCH g.member WHERE g.id = :guestBookId")
+    Optional<GuestBook> findByIdWithMember(@Param("guestbookId") UUID guestbookId);
+
+    boolean existsByIdAndMemberId(UUID id, UUID memberId);
+}
