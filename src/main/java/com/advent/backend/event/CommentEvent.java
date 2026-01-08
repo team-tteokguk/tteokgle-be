18d8390b10b597e8d8f6136091fb1656cb81fdcf
YEJI JUNG <yejiii1997@gmail.com>
2026-01-08 20:24:49 +0900

    fix: 코멘트 이벤트 레코더 comment 타입 변경 (#26)

--- src/main/java/com/advent/backend/event/CommentEvent.java
+++ src/main/java/com/advent/backend/event/CommentEvent.java
@@ -1,6 +1,5 @@
 package com.advent.backend.event;
 
 import com.advent.backend.entity.Member;
-import javax.xml.stream.events.Comment;
 
-public record CommentEvent(Member commenter, Member owner, Comment comment) {}
+public record CommentEvent(Member commenter, Member owner, String comment) {}
